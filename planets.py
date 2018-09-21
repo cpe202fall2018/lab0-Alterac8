53847c67a955d9fa3c34c9bf25b86bfeffc05bd6
Adley Wong <adleywong@Adleys-MacBook-Pro.local>
2018-09-21 10:37:26 -0700

    fixed to a float

--- planets.py
+++ planets.py
@@ -1,5 +1,5 @@
 def weight_on_planets():
-   earthWeight = int(input("What do you weigh on earth? "))
+   earthWeight = float(input("What do you weigh on earth? "))
 
    print("\nOn Mars you would weigh %s pounds.\nOn Jupiter you would weigh %s pounds." % (earthWeight*0.38, earthWeight*2.34))
    
